02048b259588fae26828688d8224e87363503c81
Leandro Ciric <lecirics@gmail.com>
2021-05-27 18:44:20 -0300

    fix: changed port to heroku default ports

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ app.get("/", (request, response) => {
   return response.json({ message: "Dont be sedentary" });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("Server started on port 3333!");
 });
