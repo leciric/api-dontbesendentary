547b7c3321adba03b202ae135887664595112c43
Leandro Ciric <lecirics@gmail.com>
2021-05-27 18:50:27 -0300

    feat: added a new route to return challenges

--- dist/server.js
+++ dist/server.js
@@ -4,10 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var challenges_1 = require("./challenges");
 var app = express_1.default();
 app.get("/", function (request, response) {
-    return response.json({ message: "Dont be sedentary" });
+    return response.json({ message: "Welcome to Dont be sedentary api" });
 });
-app.listen(3333, function () {
+app.get("/challenges", function (request, response) {
+    return response.json(challenges_1.Challenges);
+});
+app.listen(process.env.PORT || 3333, function () {
     console.log("Server started on port 3333!");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
 
+import { Challenges } from './challenges'
+
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Dont be sedentary" });
+  return response.json({ message: "Welcome to Dont be sedentary api" });
 });
+
+
+app.get("/challenges", (request, response) => {
+  return response.json(Challenges)
+})
 
 app.listen(process.env.PORT || 3333, () => {
   console.log("Server started on port 3333!");
